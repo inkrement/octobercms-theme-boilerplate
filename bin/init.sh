f0f3823a11607f7256935645f41702f8dda8c929
unknown <emil.smith@Emil-PC.bpl.local>
2015-08-18 16:29:44 +0100

    dir structure

--- bin/init.sh
+++ bin/init.sh
@@ -1,14 +1,2 @@
-mkdir assets
-mkdir assets/css
-mkdir assets/js
-mkdir assets/images
-mkdir assets/fonts
-mkdir src
-mkdir src/css
-mkdir src/js
-mkdir src/sass
-touch src/sass/main.scss
-touch src/js/main.js
-mkdir pages
-mkdir layouts
 npm install
+gulp
